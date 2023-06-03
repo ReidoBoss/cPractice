9aacf3282ec318c0df0d7b5d1137eaa53b23ea64
ReidoBoss <bryansagarino222@gmail.com>
2023-06-04 01:08:56 +0800

    henlo test

---
+++ HelloWorld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+int main(){
+
+    int op;
+
+
+    printf("Welcome to the Calculator mini!\nPress (1) for '+' \nPress (2) for '-' \nPress (3) for 'x' \nPress (4) for '/'");
+
+
+
+    return 0;
+
+}
