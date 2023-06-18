25c713e2a633badc2ae8e7362f977b0f38f392f3
ReidoBoss <bryansagarino222@gmail.com>
2023-06-19 03:22:06 +0800

    .

--- C Data Structure/Linked List/linkedlist.c
+++ C Data Structure/Linked List/linkedlist.c
@@ -9,17 +9,44 @@ typedef struct Node{
 
 }Node;
 
+void insertEnd(Node** root,int value){
+    Node* new_node = malloc(sizeof(Node));
+    if(new_node == NULL){
+        exit(1);
+    }
+    new_node -> next = NULL;
+    new_node -> x = value;
+
+    Node* curr = *root;
+    while(curr -> next !=NULL){
+        curr = curr -> next;
+    }
+}
+
 int main(){
 
     Node root;
     root.x = 15;
     root.next = malloc(sizeof(Node));
-    root.next->x = 2;
-    root.next->next = NULL;
+    root.next->x = -2;
+    root.next->next = malloc(sizeof(Node));
+    root.next->next->x = 25;
+    root.next->next->next= NULL;
 
-    printf("%d\n",root.x);
-    printf("%d\n",root.next->x);
+    // Node* curr = &root;
+    // while(curr != NULL) {
+    
+    //     printf("%d\n",curr->x);
+        
+    // }
+    // for(Node* curr = &root; curr!=NULL; curr -> next){
+    //     printf("%d->",curr->x);
+    //     curr = curr -> next;
+    // }
+    printf("null");
 
+  
+    free(root.next->next);
     free(root.next);
 
     return 0;
