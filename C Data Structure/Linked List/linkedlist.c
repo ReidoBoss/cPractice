d27bece224d3dc644ac04ce22f649364a8637f6f
ReidoBoss <bryansagarino222@gmail.com>
2023-06-20 21:25:00 +0800

    linkedlist insertEnd

--- C Data Structure/Linked List/linkedlist.c
+++ C Data Structure/Linked List/linkedlist.c
@@ -1,53 +1,63 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-typedef struct Node{
-    
+typedef struct Node
+{
+
     int x;
     struct Node* next;
 
 }Node;
 
-void insertEnd(Node** root,int value){
+void insertEnd(Node** root, int value){
     Node* new_node = malloc(sizeof(Node));
-    if(new_node == NULL){
-        exit(1);
-    }
+    new_node ->x = value;
     new_node -> next = NULL;
-    new_node -> x = value;
-
+    
+    /*
+    if a node kay empty, mahimo siya ug silbeng new node nga similar ra sa pag add ug 
+    kinaunhan nga node. kay empty man siya so maong kinaunhan ra siya kutob. 
+    */
+    if(*root == NULL){
+        *root = new_node;
+        return;
+    }  
+    /*
+    if ang node kay dili empty, mobutang siyag bag-o nga node nga pwede i insert as a new node.
+    para ma linked sila.    
+    */       
     Node* curr = *root;
-    while(curr -> next !=NULL){
-        curr = curr -> next;
+    while(curr->next != NULL){
+
+        curr = curr->next;    
+    }
+    curr->next = new_node;
+
+}
+void display(Node* root){
+    while(root!=NULL){
+        printf("%d -> ",root->x);
+        root = root->next;
+        if(root==NULL){
+            printf("null\n");
+        }
     }
 }
 
 int main(){
+    Node*root = NULL;
+    Node* curr = root;
 
-    Node root;
-    root.x = 15;
-    root.next = malloc(sizeof(Node));
-    root.next->x = -2;
-    root.next->next = malloc(sizeof(Node));
-    root.next->next->x = 25;
-    root.next->next->next= NULL;
+    insertEnd(&root,12);
+    insertEnd(&root,13);
+    insertEnd(&root,24);
+    insertEnd(&root,33);
+    insertEnd(&root,64);    
 
-    // Node* curr = &root;
-    // while(curr != NULL) {
-    
-    //     printf("%d\n",curr->x);
-        
-    // }
-    // for(Node* curr = &root; curr!=NULL; curr -> next){
-    //     printf("%d->",curr->x);
-    //     curr = curr -> next;
-    // }
-    printf("null");
 
-  
-    free(root.next->next);
-    free(root.next);
+
+
+
 
     return 0;
 
